862caced5cd8fd93dc9770e48cdeaec5c03975be
saarock <saarock4646@gmail.com>
2025-12-21 18:20:45 +0545

    solve error

--- src/sections/StudentProjectShowCase/StudentProjectShowCase.tsx
+++ src/sections/StudentProjectShowCase/StudentProjectShowCase.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
-import { Input } from "@/components/ui/input";
+import { Button } from "../../components/ui/button";
+import { Card } from "../../components/ui/card";
+import { Badge } from "../../components/ui/badge";
+import { Input } from "../../components/ui/input";
 import {
   Search,
   Rocket,
